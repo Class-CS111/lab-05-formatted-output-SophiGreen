b9b0f42d8c71ec4a27d32e775589803b769819a1
SophiGreen <phiainfinity@gmail.com>
2025-04-09 23:10:12 +0000

    commit

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
+// STUDENT NAME:Sophia Platas
 // COLLABORATORS: <tutors, instructor, or anyone else who helped you>
-
+// DATE: 4/9/2025
 // DESCRIPTION: <a sentence about what the purpose of this program is>
 
 public class Main
@@ -7,11 +8,27 @@ public class Main
 	public static void main(String[] args)
 	{
     //DECLARATION + INITIALIZATION SECTION
+    double initialAmount = 1.68; 
+    int = centsRemaining = 0;
+    int = numQuarters = 0;
+    int = numDimes = 0;
+    int = numNickles = 0;
+
 
     //CALCULATION SECTION
+    centsRemaining = (int) (100 * initialAmount);
+    numQuarters = centsRemaining / 25;
+    centsRemaining %= 25;
+    numDimes = centsRemaining / 10;
+    centsRemaining %= 10;
+    numNickles = centsRemaining / 5;
+    centsRemaining %= 5;
+
+
 
     //OUTPUT SECTION
-
+     System.out.printf("$%.2f can be coverted to %d quarters, %d dimes, %d nickles, and % pennies.",initialAmount,numQuarters,numDimes,numNickles,centsRemaining);
+    
   }
 
 }
